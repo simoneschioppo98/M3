2bc655a0d976de388e4f90601ab960778ac8a264
Simone Schioppo <simoneschioppo@MBP-di-Simone.station>
2023-04-21 20:30:10 +0200

    primo esercizio modulo 3

---
+++ D1-M3/mio.js
@@ -0,0 +1,5 @@
+window.onload = function remover {
+    myAside = document.getElementsByTagName('aside')
+    myLink = myAside.querySelectorALl('div:nth-of-type(3)')
+    myTwitter = myLynk.querySelectorAll('a:nth-of-type(2)')
+}
